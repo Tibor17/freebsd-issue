00f910406c1ae0453142f8bfca42e4d9d01c719f
Tibor17 <tibor17@lycos.com>
2017-03-01 01:19:29 +0100

    /bin/sh -c "sleep 3"

--- src/main/java/Exec.java
+++ src/main/java/Exec.java
@@ -1,12 +1,19 @@
+import java.io.File;
+
 public class Exec {
     public static void main(String... args) throws Exception {
         System.out.println("This Java application starts sleep sub-process waiting 3 seconds.");
-        System.out.printf("%,3d millis Before forking sub-process.\n", System.currentTimeMillis());
-        Process process = Runtime.getRuntime().exec("/bin/sh -c \"sleep 3\"");
-        System.out.printf("%,3d millis After Runtime.exec() method has returned.\n", System.currentTimeMillis());
+        System.out.printf("%,3d millis Before ProcessBuilder.start().\n", System.currentTimeMillis());
+        String[] cmd = {"/bin/sh", "-c", "sleep 3"};
+        File userDir = new File(System.getProperty("user.dir")).getCanonicalFile();
+        Process process = new ProcessBuilder(cmd)
+                                  .directory(userDir)
+                                  .start();
+        System.out.printf("%,3d millis After ProcessBuilder.start() method has returned.\n", System.currentTimeMillis());
         process.getOutputStream();
         process.getInputStream();
         process.waitFor();
-        System.out.printf("%,3d millis After sub-process has completed.\n", System.currentTimeMillis());
+        System.out.printf("%,3d millis After sub-process has completed with error code: %d.\n",
+                                 System.currentTimeMillis(), process.exitValue());
     }
 }

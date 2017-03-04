1ca62f34e44d141d50194992c189cf5a1ba0f8b9
Tibor17 <tibor17@lycos.com>
2017-03-04 21:22:10 +0100

    Java Logger API

--- src/main/java/Fork.java
+++ src/main/java/Fork.java
@@ -1,13 +1,34 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Semaphore;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import static java.lang.System.getProperty;
+import static java.lang.System.nanoTime;
 
 public class Fork
 {
     private static final String LINE = "01234567890123456789012345678901234567890123456789\n";
+    private static final Logger LOG = Logger.getLogger(Fork.class.getName());
+
+    static {
+        try {
+            File log = new File(getProperty("user.dir"), nanoTime() + ".log");
+            FileHandler fh = new FileHandler(log.getCanonicalPath());
+            LOG.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+        } catch (IOException e) {
+            throw new IllegalStateException(e.getLocalizedMessage(), e);
+        }
+    }
 
     public static void main(String... args) throws Exception {
+        LOG.info(System.currentTimeMillis() + " main() started");
+
         for (int i = 0; i < 200; i++) {
-            // 50  200 chars = 10 000 characters
+            // 50 x 200 chars = 10 000 characters
             System.out.print(LINE);
             System.out.flush();
         }
